8ca3df04e64a70345da94cc1eea38b90e0a6967f
vanalex <apjorquera@gmail.com>
2019-01-11 17:07:43 +0100

    more numpy examples

--- numpy_examples.py
+++ numpy_examples.py
@@ -45,5 +45,51 @@ print(nums[2:])     # Get a slice from index 2 to the end; prints "[2, 3, 4]"
 print(nums[:2])    # Get a slice from the start to index 2 (exclusive); prints "[0, 1]"
 print(nums[:])      # Get a slice of the whole list; prints ["0, 1, 2, 3, 4]"
 print(nums[:-1])    # Slice indices can be negative; prints ["0, 1, 2, 3]"
-nums[2:4] = [8, 9] # Assign a new sublist to a slice
+#nums[2:4] = [8, 9] # Assign a new sublist to a slice
 print(nums)         # Prints "[0, 1, 8, 9, 4]"
+
+animals = ['cat', 'dog', 'monkey']
+for animal in animals:
+    print(animal)
+
+animals = ['cat', 'dog', 'monkey']
+for idx, animal in enumerate(animals):
+    print('#%d: %s' % (idx + 1, animal))
+# Prints "#1: cat", "#2: dog", "#3: monkey", each on its own line
+
+nums = [0, 1, 2, 3, 4]
+squares = []
+for x in nums:
+    squares.append(x ** 2)
+print(squares)   # Prints [0, 1, 4, 9, 16]
+
+nums = [0, 1, 2, 3, 4]
+squares = [x ** 2 for x in nums]
+print(squares)   # Prints [0, 1, 4, 9, 16]
+
+nums = [0, 1, 2, 3, 4]
+even_squares = [x ** 2 for x in nums if x % 2 == 0]
+print(even_squares)  # Prints "[0, 4, 16]"
+
+d = {'cat': 'cute', 'dog': 'furry'}  # Create a new dictionary with some data
+print(d['cat'])       # Get an entry from a dictionary; prints "cute"
+print('cat' in d)     # Check if a dictionary has a given key; prints "True"
+d['fish'] = 'wet'     # Set an entry in a dictionary
+print(d['fish'])      # Prints "wet"
+# print(d['monkey'])  # KeyError: 'monkey' not a key of d
+print(d.get('monkey', 'N/A'))  # Get an element with a default; prints "N/A"
+print(d.get('fish', 'N/A'))    # Get an element with a default; prints "wet"
+del d['fish']         # Remove an element from a dictionary
+print(d.get('fish', 'N/A')) # "fish" is no longer a key; prints "N/A"
+
+d = {'person': 2, 'cat': 4, 'spider': 8}
+for animal in d:
+    legs = d[animal]
+    print('A %s has %d legs' % (animal, legs))
+# Prints "A person has 2 legs", "A cat has 4 legs", "A spider has 8 legs"
+
+d = {'person': 2, 'cat': 4, 'spider': 8}
+for animal, legs in d.items():
+    print('A %s has %d legs' % (animal, legs))
+# Prints "A person has 2 legs", "A cat has 4 legs", "A spider has 8 legs"
+
